02987e4923e6296d194f4a0b2ec9642cccb35bee
SK <noreply@github.com>
2023-08-25 19:08:43 -0700

    Completed CSS for the reservation layout

--- src/App.js
+++ src/App.js
@@ -149,131 +149,150 @@ function ReservationDetails () {
   )
 }
 
-function ReservationFinalize() {
+function ReservationFinalize () {
   return (
-    <>
-      <header>
-        <img src={logo} alt="Little Lemon Logo" />
-      </header>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/menu">Menu</a>
-          </li>
-          <li>
-            <a href="/reservations">Reservations</a>
-          </li>
-          <li>
-            <a href="/order">Order Online</a>
-          </li>
-          <li>
-            <a href="/login">Login</a>
-          </li>
-        </ul>
-      </nav>
+    <div class="wrapper">
+      <div class="container">
+        <div class="header">
+          <header>
+            <img className="logo" src={logo} alt="Little Lemon Logo" />
+          </header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/menu">Menu</a>
+              </li>
+              <li>
+                <a href="/reservations">Reservations</a>
+              </li>
+              <li>
+                <a href="/order">Order Online</a>
+              </li>
+              <li>
+                <a href="/login">Login</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
       <main>
         <section id="hero">
-          <div id='reserve-heading'>
-            <nav>
-              <a href="/">
-                <img src={backButtonBlk} alt="Back button" />
-              </a>
-            </nav>
-            <h1>Reserve Table</h1>
-            <h3>Enter customer contact information, credit card number, and then confirm reservation below</h3>
-          </div>
-          <div>
-            <img src={tablePhoto} alt="Little Lemon restaurant dining area" />
+          <div class="container">
+            <div id="hero-container">
+              <div id='reserve-heading'>
+                <div class="hero-nav">
+                  <nav>
+                    <a href="/">
+                      <img src={backButtonBlk} alt="Back button" />
+                    </a>
+                  </nav>
+                </div>
+                <div class="hero-heading">
+                  <h1>Reserve Table</h1>
+                  <h3>Enter customer contact information, credit card number, and then confirm reservation below</h3>
+                </div>
+              </div>
+              <img className="heroImage" src={tablePhoto} alt="Little Lemon restaurant dining area" />
+            </div>
           </div>
         </section>
-        <section id="reservation-contact">
-          <div id="reservation-details-fields">
-            <div className='reservation-details-field'>
-              <label>
-                <img src={dish} alt="dish icon" />
-              </label>
-              <h3>August 21, 2023</h3> 
-            </div>
-            <div className='reservation-details-field'>
-              <label>
-                <img src={clock} alt="clock icon" />
-              </label>
-              <h3>7:30 PM</h3> 
-            </div>
-            <div className='reservation-details-field'>
-              <label>
-                <img src={user} alt="user icon" />
-              </label>
-              <h3>4 Diners</h3> 
-            </div>
-            <img src={happyChef} alt="happy chef" />
+        <div className="container">
+          <div className="reservation-contact-container">
+            <section id="reservation-contact">
+              <div id="reservation-details-fields">
+                <div className='reservation-details-field'>
+                  <label>
+                    <img src={dish} alt="dish icon" />
+                  </label>
+                  <h3>August 21, 2023</h3> 
+                </div>
+                <div className='reservation-details-field'>
+                  <label>
+                    <img src={clock} alt="clock icon" />
+                  </label>
+                  <h3>7:30 PM</h3> 
+                </div>
+                <div className='reservation-details-field'>
+                  <label>
+                    <img src={user} alt="user icon" />
+                  </label>
+                  <h3>4 Diners</h3> 
+                </div>
+                <img src={happyChef} alt="happy chef" />
+              </div>
+            </section>
+            <form>
+              <fieldset>
+                <div className="field">
+                  <label for="firstName">First name</label>
+                  <input type="text" id="firstName" name="firstName" />
+                </div>
+                <div className="field">
+                  <label for="lastName">Last name</label>
+                  <input type="text" id="lastName" name="lastName" />
+                </div>
+                <div className="field">
+                  <label for="email">Email Address</label>
+                  <input type="email" id="email" name="email" />
+                </div>
+                <div id="credit-card-details">
+                  <div className="field">
+                    <label for="creditCard">Credit Card Number</label>
+                    <input type="text" id="creditCard" name="creditCard" />
+                  </div>
+                  <div className="field">
+                    <label for="creditCardCvv">CVV</label>
+                    <input type="text" id="creditCardCvv" name="creditCardCvv" />
+                  </div>
+                </div>
+                <div className='submit-btn-container'>
+                  <span></span>
+                  <button className='submit-btn'>Reserve</button>
+                </div>
+              </fieldset>
+            </form> 
           </div>
-        </section>
-        <form>
-          <fieldset>
-            <div className="field">
-              <label for="firstName">First name</label>
-              <input type="text" id="firstName" name="firstName" />
-            </div>
-            <div className="field">
-              <label for="lastName">Last name</label>
-              <input type="text" id="lastName" name="lastName" />
-            </div>
-            <div className="field">
-              <label for="email">Email Address</label>
-              <input type="email" id="email" name="email" />
-            </div>
-            <div id="credit-card-details">
-              <div className="field">
-                <label for="creditCard">Credit Card Number</label>
-                <input type="text" id="creditCard" name="creditCard" />
-              </div>
-              <div className="field">
-                <label for="creditCardCvv">CVV</label>
-                <input type="text" id="creditCardCvv" name="creditCardCvv" />
-              </div>
-            </div>
-            <input type="submit" value="Confirm" />
-          </fieldset>
-        </form>
+        </div>
       </main>
-      <footer>
-        <img src={logo} alt="Little lemon logo" />
-        <nav>
-          <h3>Doormat Navigation</h3>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Menu</li>
-            <li>Reservations</li>
-            <li>Order Online</li>
-            <li>Login</li>
-          </ul>
-        </nav>
-        <nav>
-          <h3>Contact</h3>
-          <ul>
-            <li>Address</li>
-            <li>Phone number</li>
-            <li>Email</li>
-          </ul>
-        </nav>
-        <nav>
-          <h3>Social media links</h3>
-          <ul>
-            <li>Address</li>
-            <li>Phone</li>
-            <li>email</li>
-          </ul>
-        </nav>
-      </footer>
-    </>
+      <div className="footer-container">
+        <footer>
+          <img src={lemon} alt="Little lemon logo" />
+          <nav>
+            <h3>Doormat Navigation</h3>
+            <ul>
+              <li><a href="/home">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/menu">Menu</a></li>
+              <li><a href="/reservations">Reservations</a></li>
+              <li><a href="/order">Order Online</a></li>
+              <li><a href="/login">Login</a></li>
+            </ul>
+          </nav>
+          <nav>
+            <h3>Contact</h3>
+            <ul>
+              <li><a href="/address">Address</a></li>
+              <li><a href="/phone">Phone number</a></li>
+              <li><a href="/email">Email</a></li>
+            </ul>
+          </nav>
+          <nav>
+            <h3>Social media links</h3>
+            <ul>
+              <li><a href="/address">Address</a></li>
+              <li><a href="/phone">Phone number</a></li>
+              <li><a href="/email">Email</a></li>
+            </ul>
+          </nav>
+        </footer>
+      </div>
+    </div>
   )
 }
 
