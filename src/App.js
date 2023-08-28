3210bfc86b0cfc47fc0ea73c69f176fe1f63d2f2
SK <noreply@github.com>
2023-08-28 16:37:06 -0700

    Added calendar table, diners and time

--- src/App.js
+++ src/App.js
@@ -56,24 +56,110 @@ function ReservationDetails() {
             </div>
           </div>
         </section>
-        {/* <section className="reservation-details">
-          <div className="container">
-            <div className="content">
-              <div id="reservation-details-month">
-                <nav id='calendar-nav'>
-                  <button id='previous-month'>
+        <section className="container">
+          <div className="content">
+            <div id="reservation-details">
+              <div id="calender-nav">
+                <nav>
+                  <button>
                     <img src={backButtonGrn} alt="previous month" />
                   </button>
                   <h3>August 2023</h3>
-                  <button id='next-month'>
+                  <button>
                     <img src={forwardButtonGrn} alt="next month" />
                   </button>
                 </nav>
               </div>
+              <table id="reservation-table">
+                <thead>
+                  <tr>
+                    <th>Sun</th>
+                    <th>Mon</th>
+                    <th>Tue</th>
+                    <th>Wed</th>
+                    <th>Thu</th>
+                    <th>Fri</th>
+                    <th>Sat</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                  </tr>
+                  <tr>
+                    <td>6</td>
+                    <td>7</td>
+                    <td>8</td>
+                    <td>9</td>
+                    <td>10</td>
+                    <td>11</td>
+                    <td>12</td>
+                  </tr>
+                  <tr>
+                    <td>13</td>
+                    <td>14</td>
+                    <td>15</td>
+                    <td>16</td>
+                    <td>17</td>
+                    <td>18</td>
+                    <td>19</td>
+                  </tr>
+                  <tr>
+                    <td>20</td>
+                    <td>21</td>
+                    <td>22</td>
+                    <td>23</td>
+                    <td>24</td>
+                    <td>25</td>
+                    <td>26</td>
+                  </tr>
+                  <tr>
+                    <td>27</td>
+                    <td>28</td>
+                    <td>29</td>
+                    <td>30</td>
+                    <td>31</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </table>
+              <div id="diners-input">
+                <div className='field field-flex'>
+                  <label>
+                    <img src={user} alt="user" />
+                    Number of Diners
+                  </label>
+                  <div id="diners-number-picker">
+                    <button class="number-btn">-</button>
+                      <input type='number' defaultValue={2} min={1} max={20} />
+                    <button class="number-btn">+</button>
+                  </div>
+                </div> 
+              </div>
+              <div id="time-input">
+                <div className='field'>
+                  <label>
+                    <img src={clock} alt="clock" />
+                    Time
+                  </label>
+                  <select value="1930">
+                    <option value="1930">7:30 PM</option>
+                    <option value="1945">7:45 PM</option>
+                    <option value="2000">8:00 PM</option>
+                  </select>
+                </div>
+              </div>
             </div>
           </div>
 
-        </section> */}
+        </section>
       </main>
     </>
   )
